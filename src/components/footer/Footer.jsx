d24e2ffa71ae4ac79e78e1c5c453a09179063318
Marcin Bieniek <marcingim@gmail.com>
2023-07-23 22:36:45 +0200

    Add Navbar and Footer styling, test css in Next

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 const Footer = () => {
+
   return (
-    <div>
+    <div className={styles.container}>
       @2023 Techmania. All rights reserved.
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
+import styles from './Navbar.module.css'
 
 const links = [
   {
@@ -36,12 +39,20 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">techmania</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>techmania</Link>
+      <div className={styles.links}>
         {links.map(link => (
-          <Link key={link.id} href={link.url}>{link.title}</Link>
+          <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
         ))}
+        <button
+          className={styles.logout}
+          onClick={() => {
+            console.log("Logout")
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   )
