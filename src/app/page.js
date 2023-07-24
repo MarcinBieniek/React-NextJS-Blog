661cbeca0bcc4cda295419bd2ea73aed097d7260
Marcin Bieniek <marcingim@gmail.com>
2023-07-24 18:18:03 +0200

    Add main page

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,15 @@ import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      <Image src={Hero} alt="" />
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.desc}>Turn your Idea into Reality. We bring together the teams from the global tech industry.</p>
+        <button className={styles.button}>See out works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Hero image" className={styles.img}/>
+      </div>
     </div>
   )
 }
