c642ed855acc23d82612552605bd72ffed8fcde0
Marcin Bieniek <marcingim@gmail.com>
2023-07-24 15:20:57 +0200

    Test images in NextJS, add foter and hero images

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
 
   return (
     <div className={styles.container}>
-      @2023 Techmania. All rights reserved.
+      <div>
+        @2023 Techmania. All rights reserved.
+      </div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} alt={"Facebook"} className={styles.icon}/>
+        <Image src="/2.png" width={15} height={15} alt={"Instagram"} className={styles.icon}/>
+        <Image src="/3.png" width={15} height={15} alt={"Twitter"} className={styles.icon}/>
+        <Image src="/4.png" width={15} height={15} alt={"Youtube"} className={styles.icon}/>
+      </div>
     </div>
   )
 }
