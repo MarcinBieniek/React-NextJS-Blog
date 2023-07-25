820d50da48760950ed3ecb337846fa87941eeb64
Marcin Bieniek <marcingim@gmail.com>
2023-07-25 13:16:42 +0200

    Add styling for Portfolio page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import styles from './page.module.css';
+import Button from '@/components/button/button';
+import Image from 'next/image';
 
-const Category = () => {
+const Category = ({params}) => {
+
+  console.log(params)
+
   return (
-    <div>
-      Category
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#" text="See more"></Button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.image}
+            fill={true}
+            src="https://images.pexels.com/photos/17115827/pexels-photo-17115827/free-photo-of-sztuka-statua-blekitne-niebo-rzezba.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Main image"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#" text="See more"></Button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.image}
+            fill={true}
+            src="https://images.pexels.com/photos/17115827/pexels-photo-17115827/free-photo-of-sztuka-statua-blekitne-niebo-rzezba.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Main image"
+          />
+        </div>
+      </div>
     </div>
   )
 }
