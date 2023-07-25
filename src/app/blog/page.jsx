a16a3c4efe7bdf62400110fd5b083dc4c0a4248d
Marcin Bieniek <marcingim@gmail.com>
2023-07-25 13:34:31 +0200

    Add styling for Blog posts list

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className={styles.container}>
+      <Link href="/blog/testId" className={styles.postContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Post image"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.postContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Post image"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.postContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Post image"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
     </div>
   )
 }
