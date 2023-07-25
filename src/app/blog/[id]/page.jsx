8f28b48cb13201ad624edf0cb729566b1fb611e2
Marcin Bieniek <marcingim@gmail.com>
2023-07-25 13:56:12 +0200

    Add single blog post page styling

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
 
 const BlogPost = () => {
   return (
-    <div>
-      Blog post
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amer consescretur
+          </h1>
+          <p className={styles.desc}>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque
+          ipsa quae ab illo inventore veritatis et quasi architecto
+          beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+          quia voluptas sit aspernatur aut odit aut fugit.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="User image"
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Sarah Conor</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+           src="https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+           fill={true}
+           alt="Blog image"
+           className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque
+          ipsa quae ab illo inventore veritatis et quasi architecto
+          beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+          quia voluptas sit aspernatur aut odit aut fugit.
+          <br/><br/>
+        </p>
+        <p className={styles.text}>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque
+          ipsa quae ab illo inventore veritatis et quasi architecto
+          beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+          quia voluptas sit aspernatur aut odit aut fugit.
+          <br/><br/>
+        </p>
+        <p className={styles.text}>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque
+          ipsa quae ab illo inventore veritatis et quasi architecto
+          beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+          quia voluptas sit aspernatur aut odit aut fugit.
+          <br/><br/>
+        </p>
+      </div>
     </div>
   )
 }
