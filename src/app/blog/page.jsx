565d4f620af1a5741e6a2379459432f0d1ad9fb7
Marcin Bieniek <marcingim@gmail.com>
2023-07-26 14:28:01 +0200

    Add fetch data function

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,54 +3,39 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    cache: "no-store",
+  })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+
+const Blog = async () => {
+
+  const data = await getData();
+
   return (
     <div className={styles.container}>
-      <Link href="/blog/testId" className={styles.postContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Post image"
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.postContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Post image"
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.postContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Post image"
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
+      {data.map((item) =>(
+        <Link href="/blog/testId" className={styles.postContainer} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Post image"
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
