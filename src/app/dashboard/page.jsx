5658c59994de00c9eaa0f3a4ecec3a2aea3ca9e1
Marcin Bieniek <marcingim@gmail.com>
2023-08-01 22:01:55 +0200

    Add delete post function

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -77,6 +77,18 @@ const Dashboard = () => {
     }
   }
 
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: "DELETE"
+      });
+      mutate()
+      e.target.reset()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   if (session.status === "authenticated"){
     return (
       <div className={styles.container}>
@@ -90,7 +102,7 @@ const Dashboard = () => {
                 <Image src={post.img} alt="" width={200} height={100}/>
               </div>
               <h2 className={styles.postTitle}>{post.title}</h2>
-              <span className={styles.delete}>X</span>
+              <span className={styles.delete} onClick={() => handleDelete(post._id)}>X</span>
             </div>
 
           ))}
