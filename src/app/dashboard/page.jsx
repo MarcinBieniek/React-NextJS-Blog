5a05652a7187169ebe09be22232d5cbd93e2faab
Marcin Bieniek <marcingim@gmail.com>
2023-08-01 14:56:08 +0200

    Add logout function and protected routes

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import React from 'react';
 import useSWR from 'swr'
@@ -31,7 +32,9 @@ const Dashboard = () => {
   }, [])
   */
 
-  const session = useSession()
+  const session = useSession();
+
+  const router = useRouter();
 
   console.log('session is', session)
 
@@ -39,11 +42,22 @@ const Dashboard = () => {
 
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+  if (session.status === "loading") {
+    return <p>Loading...</p>
+  }
+
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login")
+  }
+
+  if (session.status === "authenticated"){
+
+    return (
+      <div>
+        Dashboard
+      </div>
+    )
+  }
 }
 
 export default Dashboard
